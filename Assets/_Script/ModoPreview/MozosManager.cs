08c416e51cb1a3d972b2349c90c00ec714e85b1e
Pibe19 <marcosalarcon213@gmail.com>
2024-06-26 20:50:04 -0300

    MozosTerminado?

--- Assets/_Script/ModoPreview/MozosManager.cs
+++ Assets/_Script/ModoPreview/MozosManager.cs
@@ -6,8 +6,7 @@ using UnityEngine.UI;
 
 public class MozosManager : MonoBehaviour
 {
-    public Canvas UIPreview;
-    public GameObject mozosPanel, mozoPrefab;
+    public GameObject mozoPrefab;
     public Button addButton, deleteButton;
     public Transform mozoContent;
     public PlacementSystem placementSystem;
@@ -27,18 +26,14 @@ public class MozosManager : MonoBehaviour
     public void AgregarMozo()
     {
         // Crear un nuevo mozo
-        Mozo newMozo = new("Mozo " + mozoCount);
+        Mozo newMozo = new("Mozo " + (mozoCount + 1));
         mozosList.Add(newMozo);
 
         // Instanciar el prefab y actualizar el texto
         GameObject newMozoGO = Instantiate(mozoPrefab, mozoContent);
 
-        var nombre = newMozoGO.GetComponentInChildren<InputField>()
-                              .GetComponentInChildren<TextMeshProUGUI>();
-        nombre.text = newMozo.name;
-
          // Obtener el número total de mesas
-        var mesasActuales = placementSystem.GetTablesCount();
+        int mesasActuales = placementSystem.GetTablesCount();
 
         // Limpiar las asignaciones anteriores
         foreach (var mozo in mozosList)
@@ -50,17 +45,38 @@ public class MozosManager : MonoBehaviour
         for (int i = 0; i < mesasActuales; i++)
         {
             int mozoIndex = i % mozosList.Count;
-            mozosList[mozoIndex].mesasAsignadas.Add(i + 1);
+            mozosList[mozoIndex].mesasAsignadas.Add(i);
         }
 
         // Actualizar el texto de las mesas asignadas para cada mozo
         for (int i = 0; i < mozosList.Count; i++)
         {
             var mozoGO = mozoContent.GetChild(i).gameObject;
-            var mesasAsignadas = mozoGO.GetComponentInChildren<TextMeshProUGUI>();
 
-            string mesasTexto = "Mesas: " + string.Join(" ", mozosList[i].mesasAsignadas.Select(m => "mesa " + m));
-            mesasAsignadas.text = mesasTexto;
+            // Encontrar los componentes TextMeshProUGUI específicos
+            var nombreMozoTransform = mozoGO.transform.Find("NombreMozo");
+            var mesasAsignadasTransform = mozoGO.transform.Find("MesasAsignadas");
+
+            if (nombreMozoTransform != null && mesasAsignadasTransform != null)
+            {
+                var nombreMozoText = nombreMozoTransform.GetComponentInChildren<TextMeshProUGUI>();
+                var mesasAsignadasText = mesasAsignadasTransform.GetComponent<TextMeshProUGUI>();
+
+                if (nombreMozoText != null)
+                {
+                    nombreMozoText.text = mozosList[i].name;
+                }
+
+                if (mesasAsignadasText != null)
+                {
+                    string mesasTexto = "Mesas: " + string.Join(" ", mozosList[i].mesasAsignadas.Select(m => " Mesa " + (m + 1) + " "));
+                    mesasAsignadasText.text = mesasTexto;
+                }
+            }
+            else
+            {
+                Debug.LogError("TextMeshProUGUI no encontrado en el prefab de mozo.");
+            }
         }
 
         mozoCount++;
